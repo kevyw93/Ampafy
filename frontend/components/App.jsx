0b71b116ad3d04080bf3fe78614152c3f7d385fa
Kev <kevyw93@gmail.com>
2018-01-02 14:49:01 -0500

    Created Columns for Login/signup css

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import SessionFormContainer from './session/session_form_container';
 import GreetingContainer from "./greeting/greeting_container";
 import { AuthRoute } from "../util/route_util";
-import {Switch} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 
 const App = () => {
   return (
     <div>
       <Switch>
-        <AuthRoute exact path="/" component={GreetingContainer} />
+        <Route exact path="/" component={GreetingContainer} />
         <AuthRoute path='/login' component={SessionFormContainer} />
         <AuthRoute path='/signup' component={SessionFormContainer} />
       </Switch>
