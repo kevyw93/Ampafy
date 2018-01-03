b25d7a811c9d3d552c4f94a0e4c55bcf44dd02c8
Kev <kevyw93@gmail.com>
2018-01-03 13:30:18 -0500

    Fixed some css

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -14,19 +14,22 @@ const Landing = (props) => {
   }else {
     return (
       <div className="landing">
+        <div className="wrapper-opacity">
+
       <div className="button-container">
-        <h1 className="logo"><img></img>Sandrafy</h1>
+          <h1 className="logo"><img></img>Sandrafy</h1>
         <Link className="landing-signup" to="/signup">Sign Up</Link>
       <h1 className="loggedin">Already have an Account</h1>
         <Link className="landing-login" to="/login">Log In</Link>
       </div>
+      <div className="right-border">
+      </div>
       <div className="side-container">
-        <div className="side-words" >
           <h1>Get Sandrafied with the right music.</h1>
           <h2>Best Songs Out There Especially to KTV</h2>
           <h3><img></img>Search and discover how to be sandrafy</h3>
           <h3><img></img>Create playlist of your favorite music</h3>
-        </div>
+      </div>
       </div>
     </div>
     );
