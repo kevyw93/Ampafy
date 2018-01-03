7c3be379fce932e0900e1e6d2b8f104e2048bc1a
Kevin Wu <kevwu93@gmail.com>
2018-01-03 16:44:08 -0500

    Working on getting seeding working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103203739) do
+ActiveRecord::Schema.define(version: 20180103210800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20180103203739) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "song_file_name"
+    t.string "song_content_type"
+    t.integer "song_file_size"
+    t.datetime "song_updated_at"
   end
 
   create_table "songs", force: :cascade do |t|
