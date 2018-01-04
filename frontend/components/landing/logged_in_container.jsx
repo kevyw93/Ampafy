7f918ae23a5e5f7c43da2e15e263618b0effc463
Kev <kevyw93@gmail.com>
2018-01-03 23:52:28 -0500

    TopNavBar created

--- frontend/components/landing/logged_in_container.jsx
+++ frontend/components/landing/logged_in_container.jsx
@@ -3,7 +3,6 @@ import LoggedIn from "./logged_in";
 import {logout} from "../../actions/session_actions";
 
 const mapStateToProps = (state, ownProps) => {
-  debugger
   return {
     currentUser: state.session.currentUser
   };
