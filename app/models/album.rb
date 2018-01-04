a83eae7833e96ffb65e93fe0fc4bfebd154c991b
Kevin Wu <kevwu93@gmail.com>
2018-01-04 14:33:50 -0500

    Fixing merge

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   has_attached_file :album_cover
   validates :title, :author, presence: true
   validates_attachment :album_cover, presence: true,
-    content_type: { content_type: "image/jpeg"},
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
     size: { in: 0..10.megabytes }
 
   has_many :songs
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Album.destroy_all
+Song.destroy_all
 
 alb_img = File.open('../Ampafy_Seed/Album_Pics/Bright.jpg')
 album1 = Album.new(title: 'Bright', author: 'Machine Gun Kelley' )
@@ -13,7 +14,7 @@ album1.album_cover = alb_img
 album1.save!
 
 
-
-# song1 = Song.new('Home', '')
-# img = File.open('../')
-# song1.image = img
+song_1 = File.open('../Ampafy_Seed/Songs/Machine_Gun_Kelly_X_Ambassadors_Bebe_Rexha_-_Home_from_Bright_The_Album_Music_Video[Mp3Converter.net].mp3')
+song1 = Song.new(title: 'Home', album_id: album1.id)
+song1.audio = song_1
+song1.save!
