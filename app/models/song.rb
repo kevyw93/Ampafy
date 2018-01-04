c690f1da269a6c4b84bf033ba8bc78f8710d9b59
Kev <kevyw93@gmail.com>
2018-01-04 14:33:50 -0500

    Fixing merge

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ApplicationRecord
+  has_attached_file :audio
   validates :title, :album_id, presence: true
-
+  validates_attachment :audio, presence: true,
+    content_type: { content_type: 'audio'}
   belongs_to :album
 end
