bd4fa423ce338f7e9e4adbb93c2c5993259646e9
Kevin Wu <kevwu93@gmail.com>
2018-01-05 16:16:07 -0500

    Fixed errors

--- frontend/reducers/album_reducer.js
+++ frontend/reducers/album_reducer.js
@@ -4,7 +4,6 @@ import merge from 'lodash/merge';
 const albumReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_ALL_ALBUMS:
-    debugger
       return action.albums;
     default:
       return state;
