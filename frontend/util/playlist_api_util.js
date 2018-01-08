aba5e90e0c9a6cb5ef9d03606c2f19f421b5c55a
Kev <kevyw93@gmail.com>
2018-01-08 09:29:35 -0500

    Created actions reducer and api of CRUD playlist

--- frontend/util/playlist_api_util.js
+++ frontend/util/playlist_api_util.js
@@ -1,5 +1,36 @@
-export const createPlaylist = () => {
+export const createPlaylist = (data) => {
   return $.ajax({
+    type: 'post',
+    url: 'api/playlists',
+    data: data
+  });
+};
 
+export const deletePlaylist = (id) => {
+  return $.ajax({
+    type: 'delete',
+    url: `api/playlists/${id}`
+  });
+};
+
+export const updatePlaylist = (data) => {
+  return $.ajax({
+    type: 'delete',
+    url: `api/playlists/${data.id}`,
+    data: data
+  });
+};
+
+export const fetchPlaylist = (id) => {
+  return $.ajax({
+    type: 'get',
+    url: `api/playlists/${id}`
+  });
+};
+
+export const fetchAllPlaylist = () => {
+  return $.ajax({
+    type: 'get',
+    url: `api/playlists`
   });
 };
