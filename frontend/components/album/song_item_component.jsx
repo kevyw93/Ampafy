06df94649d9237eb5d32f8ee957a9ffde31d2771
Kevin Wu <kevwu93@gmail.com>
2018-01-11 03:09:47 -0500

    Created Drop Down for adding playlist and deleting playlist

--- frontend/components/album/song_item_component.jsx
+++ frontend/components/album/song_item_component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Modal from 'react-modal';
+import NewPlaylistForm from './new_playlist_form';
+import PlaylistDropDownContainer from '../playlist/playlist_drop_down_container';
 
 class SongItemComponent extends React.Component{
   constructor(props){
     super(props);
     this.handleAdd = this.handleAdd.bind(this);
     this.handleOpenClose = this.handleOpenClose.bind(this);
+    this.handleOpenCloseNewForm = this.handleOpenCloseNewForm.bind(this);
+    this.state = {bool: false};
   }
 
   handleAddSong(songId, playlistId) {
@@ -13,6 +16,12 @@ class SongItemComponent extends React.Component{
       {playlist_tagging:{song_id: songId, playlist_id: playlistId}});
       this.handleOpenClose();
     };
+  }
+
+  handleOpenCloseNewForm(){
+    let newBool = this.state.bool ? false : true;
+    let newState = { bool: newBool };
+    this.setState(newState);
   }
 
   handleOpenClose() {
@@ -24,25 +33,18 @@ class SongItemComponent extends React.Component{
 
 
   render() {
+      const form = this.props.isThreeDots ? <NewPlaylistForm
+        songId={this.props.song.id} />
+        : null;
 
     return(
       <div>
-        <button onClick={this.handleAdd(this.props.index)}>Play</button>
-      <button onClick={this.handleOpenClose}>leo</button>
-      <Modal
-        isOpen={this.props.isThreeDots}
-        ariaHideApp={false}
-        className={{
-          base: 'playlist-drop-down',
-          afterOpen: 'playlist-drop-down_after-open',
-          beforeClose: 'playlist-drop-down_before-close'
-        }}
-        overlayClassName={{
-          base: 'playlist-drop-down',
-          afterOpen: 'playlist-drop-down_after-open',
-          beforeClose: 'playlist-drop-down_before-close'
-        }}
-        >
+
+        <button onClick={this.handleAdd(this.props.index)}>{this.props.song.title}</button>
+        <h1>{this.props.author}</h1>
+      <button onClick={this.handleOpenClose}>modal</button>
+        {form}
+          {/* <PlaylistDropDownContainer handleAddSong={this.handleAddSong} handleOpenClose={this.handleOpenClose} /> */}
 
           {/* <PlaylistDropDown
             playlists={this.props.playlists}
@@ -51,7 +53,7 @@ class SongItemComponent extends React.Component{
             handleOpenClose={this.handleOpenClose}
           /> */}
 
-        </Modal>
+
       </div>
     );
       //<button onClick={this.handleOpenClose}>Three button</button>
