0419582fdc83d9c0a85eadd14f9e55d1d7ac3563
Kev <kevyw93@gmail.com>
2018-01-31 18:27:38 -0500

    Finished the whole search component

---
+++ frontend/components/album/album_search_container.js
@@ -0,0 +1,21 @@
+import {connect} from "react-redux";
+import Feature from '../main_page/feature';
+import { getAllAlbums } from "../../actions/album_actions";
+import { fetchSong } from "../../actions/song_actions";
+
+const mapStateToProps = (state) => {
+  let albumIds = state.search.albums;
+  let albums = albumIds.map(albumId => state.entities.albums[parseInt(albumId)]);
+  return {
+    albums
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getAllAlbums: () => dispatch(getAllAlbums()),
+    fetchSong: (id) => dispatch(fetchSong(id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Feature);
