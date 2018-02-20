8db434513504045bc3969124ba0759c8091e4367
Kev <kevwu93@gmail.com>
2018-02-20 15:41:13 -0500

    added a increment for current song index to que songs

--- frontend/actions/player_actions.js
+++ frontend/actions/player_actions.js
@@ -2,11 +2,18 @@ export const RECEIVE_CURRENT_SONG = 'RECEIVE_CURRENT_SONG';
 export const RECEIVE_ALBUM_SONGS = 'RECEIVE_ALBUM_SONGS';
 export const RECEIVE_CURRENT_SONG_ID = 'RECEIVE_CURRENT_SONG_ID';
 export const RECEIVE_QUE_LENGTH = 'RECEIVE_QUE_LENGTH';
+export const RECEIVE_CURRENT_SONG_INDEX = 'RECEIVE_CURRENT_SONG_INDEX';
 
 export const receiveCurrentSong = (songId) => {
   return {
     type: RECEIVE_CURRENT_SONG,
     songId
+  };
+};
+
+export const incrementCurrentSongIndex = () => {
+  return {
+    type: RECEIVE_CURRENT_SONG_INDEX
   };
 };
 
