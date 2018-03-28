67468be06e94e5a35d846aa7cdb7b72dd82b3d51
kevwu93 <kevwu93@gmail.com>
2018-03-28 13:34:27 -0400

    attempt on fixing minified code outside of env

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var config = require('config');
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
@@ -21,6 +23,15 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
+    alias: {
+       config: path.join(__dirname, 'config', process.env.NODE_ENV)
+   },
     extensions: ['.js', '.jsx', '*']
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV'])
+  ]
 };
