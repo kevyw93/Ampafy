8c38c9783910a864096e740d8ea276a28dad5629
kevwu93 <kevwu93@gmail.com>
2018-04-10 11:56:17 -0400

    logger

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,5 @@
 var path = require('path');
 var webpack = require("webpack");
-var plugins = [];
-var devPlugins = [];
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
 
 module.exports = {
   context: __dirname,
@@ -27,7 +8,6 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: './bundle.js',
   },
-  plugins: plugins,
   module: {
     loaders: [
       {
@@ -44,5 +24,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
-
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || [])
+    }),
+  ]
 };
