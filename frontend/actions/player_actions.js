518f37e2b7d9832172d89e9480bc9c69fa34a664
kevwu93 <kevwu93@gmail.com>
2018-04-12 21:53:11 -0400

    added a decrement song index action

--- frontend/actions/player_actions.js
+++ frontend/actions/player_actions.js
@@ -2,6 +2,7 @@ export const RECEIVE_CURRENT_SONG = 'RECEIVE_CURRENT_SONG';
 export const RECEIVE_QUE_LENGTH = 'RECEIVE_QUE_LENGTH';
 
 export const INCREMENT_CURRENT_SONG_INDEX = 'INCREMENT_CURRENT_SONG_INDEX';
+export const DECREMENT_CURRENT_SONG_INDEX = 'DECREMENT_CURRENT_SONG_INDEX';
 
 export const receiveCurrentSong = (payload) => {
 
@@ -15,5 +16,11 @@ export const receiveCurrentSong = (payload) => {
 export const incrementCurrentSongIndex = () => {
   return {
     type: INCREMENT_CURRENT_SONG_INDEX
+  };
+};
+
+export const decrementCurrentSongIndex = () => {
+  return {
+    type: DECREMENT_CURRENT_SONG_INDEX
   };
 };
